a2d62a813964c48a9fb4aa0653e6d8a6d69dabc1
Ana <anasingh197@gmail.com>
2025-06-09 15:05:49 -0500

    Initial Commit

---
+++ PlantPal/PlantPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantPalApp.swift
+//  PlantPal
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PlantPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
