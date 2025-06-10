868c90f30062992e62c3bd5464057446583caa6d
Ana <anasingh197@gmail.com>
2025-06-10 09:29:54 -0500

    added home screen

--- PlantPal/ContentView.swift
+++ PlantPal/ContentView.swift
@@ -9,15 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.cyan
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Hello!🌱")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                    Text("Plant Pal")
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                        .background(.white)
+                        .clipShape(Capsule())
+                }
+                
+                
+                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                    Text("Personal Info")
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity)
+                        .background(.white)
+                        .clipShape(Capsule())
+                }
+            
+                
+                
+            }
+            .padding()
+            //vstack
         }
-        .padding()
+        //zstack
     }
+        //body
 }
+//struct end
 
 #Preview {
     ContentView()
