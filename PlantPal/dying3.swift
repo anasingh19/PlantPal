e40b3f0da6fa2f4f918e7de7852528d673141216
Ana <anasingh197@gmail.com>
2025-06-11 14:26:48 -0500

    added final views

---
+++ PlantPal/dying3.swift
@@ -0,0 +1,18 @@
+//
+//  dying3.swift
+//  PlantPal
+//
+//  Created by Scholar on 6/11/25.
+//
+
+import SwiftUI
+
+struct dying3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dying3()
+}
---
+++ PlantPal/dying4.swift
@@ -0,0 +1,18 @@
+//
+//  dying4.swift
+//  PlantPal
+//
+//  Created by Scholar on 6/11/25.
+//
+
+import SwiftUI
+
+struct dying4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dying4()
+}
