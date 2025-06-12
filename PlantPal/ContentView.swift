84c168af1ae6578143c86c3b5a850ba680e95213
Ana <anasingh197@gmail.com>
2025-06-12 09:57:58 -0500

    added finshing

--- PlantPal/ContentView.swift
+++ PlantPal/ContentView.swift
@@ -11,9 +11,6 @@ struct ContentView: View {
             ZStack {
                 Color.cyan
                     .edgesIgnoringSafeArea(.all)
-                
-                
-                
                 VStack {
                     Image("PlantPal (1)")
                         .resizable(resizingMode: .stretch)
@@ -24,9 +21,7 @@ struct ContentView: View {
                         .fontWeight(.bold)
                         .foregroundColor(Color.white)
                         .padding()
-                    
                     Spacer()
-                    
                          Spacer()
                          Spacer()
                     Spacer()
@@ -46,52 +41,34 @@ struct ContentView: View {
                             .clipShape(Capsule())
                        Spacer()
                     }
-                    
                         .padding()
-                         
                          NavigationLink(destination: personalInfo()) {
                         Text("Personal Info")
                             .foregroundColor(Color.black)
                             .frame(maxWidth: .infinity)
                             .background(.white)
                             .clipShape(Capsule())
-                        
-                        
-                         
                              Spacer()
-                             
-                            
-                            
                     }
-            
-                
                     Spacer()
-                
-                  
                         .padding()
-                    
                     Text("PlantPal is a wellness app that encourages better hydration by letting you care for a digital plant pet. After a quick survey about your water goals, you’ll meet your Pal and track your daily intake. Hitting your goal keeps your Pal healthy, while missing it makes them wilt. Happy hydration!")
                         .foregroundColor(Color.black)
                         .frame(maxWidth: .infinity)
-                        .background(Color.white)
+                        .background(.white)
                         .multilineTextAlignment(.center)
                     .font(.custom("Verdana", size: 18))
                     Spacer()
                     Spacer()
                         Spacer()
                     Spacer()
-                        
                 }
                 .padding()
                 //vstack
-                navigationBarHidden(true)
             }
             //zstack
-        
-           
+            .navigationBarHidden(true)
         }
-        //
-    
     }
     //body
 }
